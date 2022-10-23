6396fcdc2b5ca4f789eac6b9a65a1080bffbe592
msluddin <msluddin@gmail.com>
2022-10-23 22:38:38 +0600

    aws amplify authentication

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Navigator from './src/navigation';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import awsconfig from './src/aws-exports';
+Amplify.configure(awsconfig);
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
       <Navigator />
@@ -18,3 +22,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default withAuthenticator(App);
