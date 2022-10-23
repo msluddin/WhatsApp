83715281549b7f133702da47e806eb772bc95672
msluddin <msluddin@gmail.com>
2022-10-24 00:22:26 +0600

    aws amplify Data models

--- src/screens/ContactsScreen.js
+++ src/screens/ContactsScreen.js
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 import ContactListItem from '../components/ContactListItem';
-import chats from '../../assets/data/chats.json';
+//import chats from '../../assets/data/chats.json';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listUsers } from '../graphql/queries';
 
 const ContactsScreen = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    API.graphql(graphqlOperation(listUsers)).then((result) => {
+      setUsers(result.data?.listUsers?.items);
+    });
+  }, []);
+
   return (
     <FlatList
-      data={chats}
-      renderItem={({ item }) => <ContactListItem user={item.user} />}
+      data={users}
+      renderItem={({ item }) => <ContactListItem user={item} />}
       style={{ backgroundColor: 'white' }}
     />
   );
