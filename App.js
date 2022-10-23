f37c01219948f46a1de570b3c5dd4457999c57cd
msluddin <msluddin@gmail.com>
2022-10-23 23:10:28 +0600

    Update Authentication

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import Navigator from './src/navigation';
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import awsconfig from './src/aws-exports';
-Amplify.configure(awsconfig);
+
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
 function App() {
   return (
--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import NotImplementedScreen from '../screens/NotImplementedScreen';
 import ChatsScreen from '../screens/ChatsScreen/ChatsScreen';
 import { Ionicons, Entypo } from '@expo/vector-icons';
+import SettingsScreen from '../screens/SettingsScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -61,7 +62,7 @@ const MainTabNavigator = () => {
       />
       <Tab.Screen
         name="Settings"
-        component={NotImplementedScreen}
+        component={SettingsScreen}
         options={{
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="settings-outline" size={size} color={color} />
